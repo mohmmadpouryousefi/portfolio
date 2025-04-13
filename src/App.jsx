b7523b2ad912b486a0001ddfeb0c230f9650dc22
mohmmadpouryousefi <mohmmadpouryousefi@gmail.com>
2025-04-14 00:12:32 +0200

    feat(routing): add routing and admin pages for blog management

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import About from "./components/About";
 import Footer from "./components/Footer";
 import BackToTop from "./components/BackToTop";
 import Loading from "./components/Loading";
+import Navbar from "./components/Navbar";
+import Blog from "./pages/blog";
+import BlogPost from "./pages/blogPost";
 import "./App.css";
+import BlogEditor from "./pages/admin/BlogEditor";
+
+// Add these imports
+import Login from "./pages/admin/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -26,15 +35,38 @@ function App() {
   if (isLoading) return <Loading />;
 
   return (
-    <div className="App">
-      <Hero />
-      <About />
-      <Skills />
-      {/* <Header /> */}
-      <Footer />
-      <BackToTop />
-      {/* Other components will go here */}
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <About />
+                <Skills />
+                <Footer />
+                <BackToTop />
+              </>
+            }
+          />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<BlogPost />} />
+
+          {/* Admin routes */}
+          <Route path="/admin/login" element={<Login />} />
+          <Route
+            path="/admin/blog/new"
+            element={
+              <ProtectedRoute>
+                <BlogEditor />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
